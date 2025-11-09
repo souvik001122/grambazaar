6ac8fb30ff6a39830aa31d83688f01abac09caf1
souvik001122 <souvikdas787286@gmail.com>
2025-11-09 19:51:51 +0530

    Force enable seed endpoint - update comment

--- server/src/routes/dev.js
+++ server/src/routes/dev.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { seedDev } = require('../controllers/devController');
 
-// Allow seeding in all environments (temporary - remove after first use in production)
+// Seed endpoint enabled - will be restricted after initial production seed
 router.post('/seed', seedDev);
 
 module.exports = router;
